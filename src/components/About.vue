b94cf7eb456ad8f268c39046f1061ffa9dd8fdd5
Barbara Percy <bpercy@users.noreply.github.com>
2021-03-10 16:28:40 +0000

    Tense changes
    And add a link to the IEA

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,17 +1,15 @@
 <template>
 	<div class="about">
-		<Banner class="banner-style"
-			enTitle="About VIEWpoint"
-		></Banner>
+		<Banner class="banner-style" enTitle="About VIEWpoint"></Banner>
 		<div class="about-content">
 			<div class="bilingual">
 				<p>
 					The VIEWpoint climate science project shares the world-class
 					scientific research of the CSSP China (Climate Science for
 					Service Partnership China) project, which is a collaboration
-					with CMA, the IAP and UK institutions. Follow us to discover
-					climate data tools, demonstrators, training resources, articles
-					and videos designed for government, industry and the public.
+					with CMA, the IAP and UK institutions. Discover climate data
+					tools, demonstrators, training resources, articles and
+					videos designed for government, industry and the public.
 				</p>
 				<p lang="zh-cn">
 					VIEWpoint 气候科学项目与中国气象局（
@@ -30,46 +28,61 @@
 			</div>
 			<div class="bilingual">
 				<p>
-					Welcome to VIEWpoint, where you will be able to receive insights
-					into cutting-edge climate research and advanced information
-					about the China-UK joint research project, Climate Science for
+					Welcome to VIEWpoint, where you can find insights into
+					cutting-edge climate research and advanced information about
+					the China-UK joint research project, Climate Science for
 					Service Partnership (CSSP) China.
 				</p>
 				<p lang="zh-cn">
 					欢迎来到
 					VIEWpoint新观点，在这里您可获取有关前沿气候研究的深刻见解以及有关中英联合研究项目
-					- 中国气候科学服务合作伙伴 (CSSP) 项目的进阶咨询。
+					- 中国气候科学服务合作伙伴（CSSP）项目的进阶咨询。
 				</p>
 			</div>
 			<div class="bilingual">
 				<p>
 					This successful collaboration with the
-					<a href="https://data.cma.cn/en" target="_blank" rel="noopener noreferrer">
-						<font-awesome-icon
-							icon="external-link-alt"
-						></font-awesome-icon
-						>&nbsp;China Meteorological Administration</a
+					<a
+						href="http://www.cma.gov.cn/en2014/"
+						target="_blank"
+						rel="noopener noreferrer"
 					>
-					(CMA), the
-					<a href="http://english.iap.cas.cn/" target="_blank" rel="noopener noreferrer">
-						<font-awesome-icon
-							icon="external-link-alt"
-						></font-awesome-icon
-						>&nbsp;Institute of Atmospheric Physics at the Chinese
-						Academy of Sciences</a
+						<span class="nowrap">
+							<font-awesome-icon
+								icon="external-link-alt"
+							></font-awesome-icon>
+							China
+						</span>
+						Meteorological Administration (CMA)</a
+					>, the
+					<a
+						href="http://english.iap.cas.cn/"
+						target="_blank"
+						rel="noopener noreferrer"
 					>
-					and Chinese universities started in 2014 and has already
-					produced more than 340 published scientific research papers. UK
-					collaborators are led by the Met Office and include other key
-					organisations in the UK.
+						<span class="nowrap">
+							<font-awesome-icon
+								icon="external-link-alt"
+							></font-awesome-icon>
+							Institute
+						</span>
+						of Atmospheric Physics (IAP) at the Chinese Academy of
+						Sciences</a
+					> and Chinese universities started in 2014 and has already
+					produced more than 340 published scientific research papers.
+					UK collaborators are led by the Met Office and include other
+					key organisations in the UK.
 				</p>
 				<p lang="zh-cn">
 					本项目成功地得到了与<a
-						href="https://data.cma.cn/"
-						target="_blank" rel="noopener noreferrer"
+						href="https://www.cma.gov.cn"
+						target="_blank"
+						rel="noopener noreferrer"
 						>中国气象局</a
-					>
-					(CMA)、<a href="http://www.iap.cas.cn/" target="_blank" rel="noopener noreferrer"
+					>（CMA）、<a
+						href="http://www.iap.cas.cn/"
+						target="_blank"
+						rel="noopener noreferrer"
 						>中国科学院大气物理研究所</a
 					>（IAP）以及多所中国高等学府的支持。 它开始于 2014
 					年，到目前为止已经发表了 340
@@ -78,10 +91,22 @@
 			</div>
 			<div class="bilingual">
 				<p>
-					VIEWpoint is a CSSP China project, delivered by the Institute
-					for Environmental Analytics, based at the University of Reading,
-					UK. We are producing a wide range of high-quality materials to
-					support evidence-based decision-making to address climate risks
+					VIEWpoint is a CSSP China project, delivered by the
+					<a
+						href="https://www.the-iea.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<span class="nowrap">
+							<font-awesome-icon
+								icon="external-link-alt"
+							></font-awesome-icon>
+							Institute
+						</span>
+						for Environmental Analytics (IEA)</a
+					>, based at the University of Reading, UK. The IEA have
+					produced a wide range of high-quality materials to support
+					evidence-based decision-making to address climate risks
 					leading to lasting benefits for China.
 				</p>
 				<p lang="zh-cn">
@@ -100,10 +125,11 @@
 			<div class="bilingual">
 				<p>
 					CSSP China is made up of many projects looking at different
-					aspects of climate research relating to China’s needs and to the
-					Chinese Government’s commitment to net zero emissions by 2060.
-					Its aim is to develop partnerships to strengthen the resilience
-					of vulnerable communities to weather and climate variability.
+					aspects of climate research relating to China’s needs and to
+					the Chinese Government’s commitment to net zero emissions by
+					2060. Its aim is to develop partnerships to strengthen the
+					resilience of vulnerable communities to weather and climate
+					variability.
 				</p>
 				<p lang="zh-cn">
 					CSSP China
@@ -114,14 +140,15 @@
 			</div>
 			<div class="bilingual">
 				<p>
-					Collaborating scientists in China and in the UK meet each year
-					for a CSSP China scientific conference / workshop to share the
-					latest developments, scientific progress and achievements. In
-					past years each country has hosted the event in turn but the
-					global pandemic has restricted travel and this year’s workshop
-					was held virtually, in October. Delegates were told that
-					prototype climate services, developed by scientists in both
-					countries working together, are already being trialled in China.
+					Collaborating scientists in China and in the UK meet each
+					year for a CSSP China scientific conference / workshop to
+					share the latest developments, scientific progress and
+					achievements. In past years each country has hosted the
+					event in turn but the global pandemic has restricted travel
+					and this year’s workshop was held virtually, in October.
+					Delegates were told that prototype climate services,
+					developed by scientists in both countries working together,
+					are already being trialled in China.
 				</p>
 				<p lang="zh-cn">
 					每年中英双方的合作科学家都会出席 CSSP China
@@ -140,10 +167,11 @@
 			</div>
 			<div class="bilingual">
 				<p>
-					VIEWpoint is a CSSP China project, delivered by the Institute
-					for Environmental Analytics, based at the University of Reading,
-					UK, to share high quality resources, showcasing new data and
-					prototype climate services in China and internationally.
+					VIEWpoint is a CSSP China project, delivered by the
+					Institute for Environmental Analytics, based at the
+					University of Reading, UK, to share high quality resources,
+					showcasing new data and prototype climate services in China
+					and internationally.
 				</p>
 				<p lang="zh-cn">
 					VIEWpoint 是一个 CSSP China
@@ -152,10 +180,10 @@
 			</div>
 			<div class="bilingual">
 				<p>
-					These will include a website, a searchable catalogue of
-					published papers, data visualisations, supporting training
-					resources and a handbook. A series of written, video and audio
-					content and infographics will give insights into the research.
+					These resources include this website, a searchable catalogue
+					of published papers, data visualisations, training resources
+					and a handbook. A series of written, video and audio content
+					and infographics also give insights into the research.
 				</p>
 				<p lang="zh-cn">
 					这其中将包括网站、可检索的已发表论文的目录、数据可视化、辅助培训资源和手册。同时，一系列书面、视频和音频内容以及信息图表将为研究提供深刻见解。
@@ -163,19 +191,20 @@
 			</div>
 			<div class="bilingual">
 				<p>
-					The aim is to raise awareness of how climate change will affect
-					China and to share the newest and highest quality science and
-					data available to inform plans and strategic decisions in the
-					face of climate change. CSSP China is funded through the Newton
-					Fund and the Department for Business, Energy &amp; Industrial
-					Strategy (BEIS) UK-China Research Innovation Partnership Fund.
+					The aim is to raise awareness of how climate change will
+					affect China and to share the newest and highest quality
+					science and data available to inform plans and strategic
+					decisions in the face of climate change. CSSP China is
+					funded through the Newton Fund and the Department for
+					Business, Energy &amp; Industrial Strategy (BEIS) UK-China
+					Research Innovation Partnership Fund.
 				</p>
 				<p lang="zh-cn">
 					项目目的在于提高人们对中国气候变化的认识，并分享最新、最高品质的科学和数据，从而为应对气候变化的计划和战略决策提供信息。
 					<br />
 					<br />
-					CSSP China 由牛顿基金以及商业、能源与产业战略部 (BEIS)
-					的中英研究与创新合作基金资助。
+					CSSP China
+					由牛顿基金以及商业、能源与产业战略部（BEIS）的中英研究与创新合作基金资助。
 				</p>
 			</div>
 		</div>
@@ -215,7 +244,7 @@ export default {
 	justify-content: space-between;
 	align-items: flex-start;
 }
-h2, 
+h2,
 p {
 	width: 48%;
 }
@@ -238,6 +267,7 @@ a:lang(zh-cn) {
 	color: var(--mandarinAnchor);
 }
 a:hover,
+a:hover span,
 a:hover svg path {
 	color: var(--vpOrange);
 }
