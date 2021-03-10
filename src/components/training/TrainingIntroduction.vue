58aa04cbb68b9e9f867cfb9d4be2db40a493112f
Barbara Percy <bpercy@users.noreply.github.com>
2021-03-10 10:59:11 +0000

    Minor text changes

--- src/components/training/TrainingIntroduction.vue
+++ src/components/training/TrainingIntroduction.vue
@@ -2,7 +2,7 @@
 	<div>
 		<h1>Introduction</h1>
 		<p>
-			There are three key resources available for help with using UMEP.
+			There are three key resources available for help with using UMEP:
 			Workshop materials, tutorials and videos to allow you to follow
 			along. All of these are complimentary and have been organised to
 			streamline your user experience. The example data is available in
@@ -36,7 +36,7 @@
 		<p>
 			UMEP is a powerful tool and opens up a world of possibilities for
 			manipulating urban data. For a full list of the available tutorials
-			including Energy, Water and radiation fluxes for one location, Mean
+			including energy, water and radiation fluxes for one location, mean
 			radiation temperature modelling in complex urban settings, and other
 			things, please see the
 			<a
