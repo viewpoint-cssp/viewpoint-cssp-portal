0e76a80feebc14af479411c77648294d2e921e7d
Barbara Percy <bpercy@users.noreply.github.com>
2021-03-12 10:40:02 +0000

    Creates CNAME to deploy on www.viewpoint-cssp.org

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,17 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# create a copy of index.html 
+# create 404.html as a copy of index.html 
 cp index.html 404.html
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# deploying to our custom domain
+echo 'www.viewpoint-cssp.org' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# deploying to https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/viewpoint-cssp/viewpoint-cssp-portal.git master:gh-pages
 
 cd -
