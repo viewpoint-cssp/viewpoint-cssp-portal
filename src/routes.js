a14d3fd861298d6935d2c891bf46ea53a2be3f18
Barbara Percy <bpercy@users.noreply.github.com>
2021-03-24 08:41:46 +0000

    Hide any dropdown on page change
    Also show page in browser tab and history

--- src/routes.js
+++ src/routes.js
@@ -26,62 +26,98 @@ const router = new VueRouter({
 		{
 			path: '/about',
 			name: 'About',
-			component: About
+			component: About,
+			meta: {
+				title: 'VIEWpoint | About'
+			}
 		},
 		{
 			path: '/briefing',
 			name: 'Briefing',
-			component: Briefing
+			component: Briefing,
+			meta: {
+				title: 'VIEWpoint | Briefing notes'
+			}
 		},
 		{
 			path: '/catalogue',
 			name: 'Catalogue',
-			component: Catalogue
+			component: Catalogue,
+			meta: {
+				title: 'VIEWpoint | Catalogue'
+			}
 		},
 		{
 			path: '/demonstrators',
 			name: 'Demonstrators',
-			component: Demonstrators
+			component: Demonstrators,
+			meta: {
+				title: 'VIEWpoint | Demonstrators'
+			}
 		},
 		{
 			path: '/explainers',
 			name: 'Explainers',
-			component: Explainers
+			component: Explainers,
+			meta: {
+				title: 'VIEWpoint | Explainers'
+			}
 		},
 		{
 			path: '/glossary',
 			name: 'Glossary',
-			component: Glossary
+			component: Glossary,
+			meta: {
+				title: 'VIEWpoint | Glossary'
+			}
 		},
 		{
 			path: '/handbook',
 			name: 'Handbook',
-			component: Handbook
+			component: Handbook,
+			meta: {
+				title: 'VIEWpoint | Handbook'
+			}
 		},
 		{
 			path: '/home',
 			name: 'Home',
-			component: Home
+			component: Home,
+			meta: {
+				title: 'VIEWpoint | Home'
+			}
 		},
 		{
 			path: '/infographics',
 			name: 'Infographics',
-			component: Infographics
+			component: Infographics,
+			meta: {
+				title: 'VIEWpoint | Infographics'
+			}
 		},
 		{
 			path: '/resources',
 			name: 'Resources',
-			component: Resources
+			component: Resources,
+			meta: {
+				title: 'VIEWpoint | Resources'
+			}
 		},
 		{
 			path: '/videos',
 			name: 'Videos',
-			component: Videos
+			component: Videos,
+			meta: {
+				title: 'VIEWpoint | Videos'
+			}
 		},
 		{
 			path: '/training',
 			name: 'Training',
-			component: Training
+			component: Training,
+			meta: {
+				title: 'VIEWpoint | Training materials'
+			}
 		},
 		{
 			path: '*',
@@ -89,6 +125,71 @@ const router = new VueRouter({
 			name: 'VIEWpoint404'
 		}
 	]
+})
+
+// from https://alligator.io/vuejs/vue-router-modify-head/
+// This callback runs before every route change, including on page load.
+router.beforeEach((to, from, next) => {
+	// This goes through the matched routes from last to first, finding the closest route with a title.
+	// eg. if we have /some/deep/nested/route and /some, /deep, and /nested have titles, nested's will be chosen.
+	const nearestWithTitle = to.matched
+		.slice()
+		.reverse()
+		.find(r => r.meta && r.meta.title)
+
+	// Find the nearest route element with meta tags.
+	const nearestWithMeta = to.matched
+		.slice()
+		.reverse()
+		.find(r => r.meta && r.meta.metaTags)
+
+	// If a route with a title was found, set the document (page) title to that value.
+	if (nearestWithTitle) document.title = nearestWithTitle.meta.title
+
+	// Remove any stale meta tags from the document using the key attribute we set below.
+	Array.from(
+		document.querySelectorAll('[data-vue-router-controlled]')
+	).map(el => el.parentNode.removeChild(el))
+
+	// Skip rendering meta tags if there are none.
+	if (!nearestWithMeta) return next()
+
+	// Turn the meta tag definitions into actual elements in the head.
+	nearestWithMeta.meta.metaTags
+		.map(tagDef => {
+			const tag = document.createElement('meta')
+
+			Object.keys(tagDef).forEach(key => {
+				tag.setAttribute(key, tagDef[key])
+			})
+
+			// We use this to track which meta tags we create, so we don't interfere with other ones.
+			tag.setAttribute('data-vue-router-controlled', '')
+
+			return tag
+		})
+		// Add the meta tags to the document head.
+		.forEach(tag => document.head.appendChild(tag))
+
+	next()
+})
+
+router.afterEach(() => {
+	// if selected by a dropdown (a resources page) this will kindly remove the dropdown (rather
+	// than waiting until the cursor moves away or, in touch screens, somewhere else gets focus)
+	const navMenu = document.getElementsByClassName('nav-menu')
+	if (navMenu.length > 0) {
+		const subNav = navMenu[0].getElementsByClassName('sub-nav')
+		for (let i = 0; i < subNav.length; i++) {
+			// parent <li> is no longer being hovered over
+			subNav[i].style.display = 'none'
+			setTimeout(() => {
+				// and quickly set ul.sub-nav's display back again
+				// to its default state (which is { display: none })
+				subNav[i].style.display = ''
+			}, 500)
+		}
+	}
 })
 
 export default router
