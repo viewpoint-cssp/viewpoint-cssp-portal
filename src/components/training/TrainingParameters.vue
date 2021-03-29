e09e23fc95e524be3eecc5d2f2797f615ce925f6
Barbara Percy <bpercy@users.noreply.github.com>
2021-03-29 16:20:20 +0100

    Fix cut-and-paste typo!

--- src/components/training/TrainingParameters.vue
+++ src/components/training/TrainingParameters.vue
@@ -7,10 +7,13 @@
 		<p>
 			In this video you will learn how to:
 			<ul>
-				<li>Editing and saving a notebook</li>
-				<li>Adding and running cells</li>
-				<li>Edit/command mode</li>
-				<li>Taking notes using Markdown</li>
+				<li>
+					Create parameters including roughness length and plan area index.
+				</li>
+				<li>
+					Calculate and display the height to width ratio (H/W) using the 
+					field calculator in the attributes table.
+				</li>
 			</ul>
 		</p>
 		<p>
