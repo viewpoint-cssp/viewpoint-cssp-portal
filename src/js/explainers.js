0e84173555ac1271bbdbe54a8e6acc164e49925a
Barbara Percy <bpercy@users.noreply.github.com>
2021-03-31 14:32:55 +0100

    Final two explainers

---
+++ src/js/explainers.js
@@ -0,0 +1,146 @@
+// driver file for videos page
+export const explainerPdfs = [
+	{
+		enTitle: 'Investigating heatwaves in China under climate change',
+		enPdf: 'E01-en-investigating-heatwaves',
+		cnTitle: '气候变化的背景下的中国 热浪研究',
+		cnPdf: 'E01-cn-investigating-heatwaves',
+		imgCredit: 'Pixaby.com'
+	},
+	{
+		enTitle:
+			'Increasing flash floods in a drying climate: dual challenges facing Southwest China',
+		enPdf: 'E02-en-increasing-flash-floods',
+		cnTitle: '干旱气候中的暴洪泛滥：中国西南部地区面临的双重挑战',
+		cnPdf: 'E02-cn-increasing-flash-floods',
+		imgPosition: 'bottom',
+		imgCredit: 'Chan Xiao (NCC, CMA)',
+		imgAlt: 'Rural area in Yunnan Province, Southwest China'
+	},
+	{
+		enTitle: 'Tibetan Plateau Vortices: a comparison of tracking methods',
+		enPdf: 'E03-en-tibetan-plateau-vortices',
+		cnTitle: '青藏高原低涡跟踪调查研究：现代方法和传统方法的比较',
+		cnPdf: 'E03-cn-tibetan-plateau-vortices',
+		imgPosition: 'bottom',
+		imgCredit: 'Unsplash',
+		imgAlt: 'Tibetan Plateau'
+	},
+	{
+		enTitle: 'What will shape future Beijing haze events and air quality?',
+		enPdf: 'E04-en-beijing-haze',
+		cnTitle: '北京雾霾未来将何去何从？',
+		cnPdf: 'E04-cn-beijing-haze',
+		imgPosition: 'bottom',
+		imgCredit: 'Pixabay.com',
+		imgAlt: 'Beijing haze event at Palace Museum\n北京故宫博物院雾霾事件'
+	},
+	{
+		enTitle:
+			'Efficient assessments of Chinese Fengyun-3 satellite instruments to improve weather forecasts',
+		enPdf: 'E05-en-satellite-instrument',
+		cnTitle: '高效评估风云三号卫星仪器，借以提升天气预报质量',
+		cnPdf: 'E05-cn-satellite-instrument',
+		imgCredit: 'WikiImages, Pixabay.com',
+		imgAlt: 'Satellite image of Earth'
+	},
+	{
+		enTitle:
+			'Arup climate risk tool: protecting your infrastructure under climate change',
+		enPdf: 'E06-en-arup-climate-risk',
+		cnTitle: '奥雅纳气候风险工具：在气候变化背景下保护您的基础设施',
+		cnPdf: 'E06-cn-arup-climate-risk',
+		imgPosition: 'top',
+		imgCredit: 'Erdenebayar Bayansan, Pixabay.com'
+	},
+	{
+		enTitle: `High-resolution revolution brings insight into China's climate`,
+		enPdf: `E07-en-high-resolution`,
+		cnTitle: '高分辨率革命助力中国气候研究',
+		cnPdf: `E07-cn-high-resolution`,
+		imgCredit: 'Vitor Dutra Kaosnoff, Pixabay'
+	},
+	{
+		enTitle:
+			'Safer operation for urban rail transport: wind risk warning system',
+		enPdf: 'E08-en-urban-rail-transport',
+		cnTitle: '集成式大风预警系统，让城市轨道交通运行更安全',
+		cnPdf: 'E08-cn-urban-rail-transport',
+		imgCredit: 'PublicDomainPictures, Pixabay'
+	},
+	{
+		enTitle:
+			'A promising outlook: skilful seasonal forecasts for energy sectors in China',
+		enPdf: 'E09-en-seasonal-forecast',
+		cnTitle: '季节性预测在中国能源行业的应用前景可期',
+		cnPdf: 'E09-cn-seasonal-forecast',
+		imgCredit: 'Vista Wei, Unsplash',
+		imgAlt: 'Wind farm in Xinyang, China'
+	},
+	{
+		enTitle:
+			'Correcting urban bias in large-scale temperature records in China',
+		enPdf: 'E10-en-correcting-urban-bias',
+		cnTitle: '修正中国大规模气温记录中的城市偏差',
+		cnPdf: 'E10-cn-correcting-urban-bias',
+		imgCredit: 'wei zhu, Pixabay',
+		imgAlt: 'Shanghai'
+	},
+	{
+		enTitle:
+			'Flood footprint assessment: a new approach for impacts and recovery',
+		enPdf: 'E11-en-flood-footprint-assessment',
+		cnTitle: '洪水足迹评估：一种针对影响和恢复的全新方法',
+		cnPdf: 'E11-cn-flood-footprint-assessment',
+		imgCredit: 'Jéan Béller, Unsplash'
+	},
+	{
+		enTitle:
+			'Working together: the China Framework for Climate Services (CFCS)',
+		enPdf: 'E12-en-working-together',
+		cnTitle: '携手合作：中国气候服务框架(CFCS)',
+		cnPdf: 'E12-cn-working-together'
+	},
+	{
+		enTitle: 'Examining airflow around tall buildings using a 3D model',
+		enPdf: 'E13-en-examining-airflow',
+		cnTitle: '利用3D模型检测气流在高建筑物的流动情况',
+		cnPdf: 'E13-cn-examining-airflow',
+		imgCredit: 'Li Yang, Unsplash',
+		imgAlt: 'Beijing cityscape'
+	},
+	{
+		enTitle:
+			'Increasing summer rainfall intensity during the past four decades observed in China',
+		enPdf: 'E14-en-increasing-summer-rainfall',
+		cnTitle: '对中国近四十年来夏季降水强度增加的研究',
+		cnPdf: 'E14-cn-increasing-summer-rainfall',
+		imgPosition: 'top',
+		imgCredit: 'Pexels, Pixabay'
+	},
+	{
+		enTitle:
+			'Daily satellite monitoring of crop drought conditions: developing a service',
+		enPdf: 'E15-en-daily-satellite-monitoring',
+		cnTitle: '服务开发：作物干旱状况卫星日常监测',
+		cnPdf: 'E15-cn-daily-satellite-monitoring',
+		imgCredit: 'Pixabay',
+		imgAlt: 'Maize growing'
+	},
+	{
+		enTitle:
+			'Urgent attention needed for shift towards increasing flash droughts over China',
+		enPdf: 'E16-en-flash-droughts',
+		cnTitle: '聚焦中国日益严重的 骤发干旱事件',
+		cnPdf: 'E16-cn-flash-droughts',
+		imgCredit: 'Markus Spiske, Unsplash'
+	},
+	{
+		enTitle: 'How will extreme cold events change over Southeast China?',
+		enPdf: 'E17-en-extreme-cold',
+		cnTitle: '中国东南部极端寒冷事件的未来变化趋势',
+		cnPdf: 'E17-cn-extreme-cold',
+		imgCredit: 'Kenneth Yang, Unsplash',
+		imgAlt: 'Nanjing city after snow\n雪后的南京'
+	}
+]
